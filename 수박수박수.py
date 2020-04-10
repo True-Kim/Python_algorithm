ff3913d0c07aee34da3d59953afa89a55ac9feb9
True-Kim <lullulu10@hanmail.net>
2020-04-10 16:14:13 +0900

    수박수박수

---
+++ 수박수박수.py
@@ -0,0 +1,13 @@
+def tnqkr(n):
+    answer = '수박'
+
+    if n<=10000:
+        if n%2==0:
+            answer=answer*(n//2)
+        elif n%2!=0:
+            answer=answer*(n//2)+answer[0]
+    print(answer)
+    return(answer)
+tnqkr(3)
+tnqkr(15)
+
