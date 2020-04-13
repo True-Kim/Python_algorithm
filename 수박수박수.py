e22095381b22cd04a35522fe4ab26e4adace3030
True-Kim <53033104+True-Kim@users.noreply.github.com>
2020-04-14 00:45:13 +0900

    Update 수박수박수.py

--- 수박수박수.py
+++ 수박수박수.py
@@ -2,9 +2,9 @@ def waterMelon(n):
     answer = '수박'
     if n<=10000:
         if n%2==0:
-            answer=answer*(n//2)
+            answer = answer*(n//2)
         elif n%2!=0:
-            answer=answer*(n//2)+answer[0]
+            answer = answer*(n//2)+answer[0]
     print(answer)
     return(answer)
 waterMelon(3)
