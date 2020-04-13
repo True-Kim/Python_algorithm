4f98373426e6e131e256bd45a16cfc341676aa62
True-Kim <lullulu10@hanmail.net>
2020-04-14 00:39:50 +0900

    year

---
+++ year.py
@@ -0,0 +1,12 @@
+def solution(a, b):
+    answer = 'FRI,SAT,SUN,MON,TUE,WED,THU'
+    days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if a<=12:
+        n=a
+        sum_day=sum(days[0:n-1])+b
+        numb=sum_day%7
+    print(answer.split(",")[numb-1])
+    return answer.split(",")[numb-1]
+
+solution(5,24)
+solution(1,1)
