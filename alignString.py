1f91833f5916917e76341998e1d3b78c589ca331
True-Kim <lullulu10@hanmail.net>
2020-04-22 23:02:40 +0900

    문자열 내마음대로 정렬하기

--- alignString.py
+++ alignString.py
@@ -1,3 +1,11 @@
+# 풀이
+# 1. strings 안의 문자열의 n번째 인덱스를 찾아 index에 저장
+# 2. index의 값과 strings안의 문자열 n번째 값이 같으면 answer배열에 넣기
+#
+# 관건
+# 1. for문에서 j와 i를 비교하냐 vs i와 j를 비교하냐?
+# 2. 사전순으로 정렬이 안됨..ㅠ > 초반에 strings를 sort()로 정렬하고 시작하면 됨.
+
 def solution(strings, n):
     answer = []
     index = []
